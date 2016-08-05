c19ec3df0e40fdc2350bee1231a1ef27a62829f7
Marc Sutter <marc.sutter@swisscom.com>
2016-08-05 15:58:43 +0200

    move task related types to tasks.go file

--- pulp/distributors.go
+++ pulp/distributors.go
@@ -2,24 +2,6 @@ package pulp
 
 import ()
 
-type PublishTask struct {
-	Id             string          `json:"task_id"`
-	StartTime      string          `json:"start_time"`
-	FinishTime     string          `json:"finish_time"`
-	State          string          `json:"state"`
-	Error          *PublishError   `json:"error"`
-	ProgressReport *ProgressReport `json:"progress_report"`
-	Result         *PublishResult  `json:"result"`
-}
-
-type PublishError struct {
-	Description string `json:"description"`
-}
-
-type PublishResult struct {
-	Details []*Distributor `json:"details"`
-}
-
 type Distributor struct {
 	Description  string   `json:"description"`
 	ItemsTotal   int      `json:"items_total"`
